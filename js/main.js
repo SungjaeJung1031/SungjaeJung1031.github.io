0920e8f25b75294f772944bcb2bf89f44ad8f39d
sungjae.jung1031 <sungjae.jung1031@gmail.com>
2021-05-09 03:30:17 +0900

    update my-theme

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-$(document).ready(function() {
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
+// $(document).ready(function() {
+//     $nav = $('.nav');
+//     $toggleCollapse = $('.toggle-collapse');
 
-    /** click event on toggle menu */
-    $toggleCollapse.click(function() {
-        $nav.toggleClass('collapse');
-    });
-});
+//     /** click event on toggle menu */
+//     $toggleCollapse.click(function() {
+//         $nav.toggleClass('collapse');
+//     });
+// });
+
+const slideShow = document.getElementById('slideShow');
+const slides = slideShow.getElementByTagName('vidoe');
+var index = 0;
+
+function nextSlide() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prevSlide() {
+    slides[index].classList.remove('active');
+    index = (index + slide.lenght - 1) % slides.length;
+    slides[index].classList.add('active');
+}
